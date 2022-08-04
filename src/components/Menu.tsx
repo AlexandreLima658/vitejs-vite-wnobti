c0154e6eaf61977f871c0d38a15482a8988e3c12
AlexandreLima658 <alexandrec@alu.ufc.br>
2022-08-04 14:02:15 -0300

    1 file modified

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,34 @@
-import './Style.css'
+import './Style.css';
 
-function Menu(){
-  return(
+function Menu() {
+  return (
     <>
       <header>
         <nav>
-            <a href="#" className="logo">Logo</a>
-            <ul className="nav-list">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Products</a></li>
-              <li><a href="#">contacts</a></li>
-           </ul>  
+          <a href="#" className="logo">
+            Logo
+          </a>
+          <ul className="nav-list">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">contacts</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
         </nav>
       </header>
     </>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
