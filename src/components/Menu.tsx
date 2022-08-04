4a1b2648abdd89b0824226ddc9375df4f4994ef9
AlexandreLima658 <alexandrec@alu.ufc.br>
2022-08-04 13:58:27 -0300

    Initial commit

---
+++ src/components/Menu.tsx
@@ -0,0 +1,21 @@
+import './Style.css'
+
+function Menu(){
+  return(
+    <>
+      <header>
+        <nav>
+            <a href="#" className="logo">Logo</a>
+            <ul className="nav-list">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">contacts</a></li>
+           </ul>  
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Menu
